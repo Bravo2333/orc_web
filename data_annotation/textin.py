eabb49101fc5fa2d7e2c8cdfb868e7e00659c3bc
Bravo2333 <2087369435@qq.com>
2024-10-15 20:38:44 +0800

    update

--- data_annotation/textin.py
+++ data_annotation/textin.py
@@ -8,14 +8,14 @@ def get_file_content(filePath):
 
 
 class CommonOcr(object):
-    def __init__(self, image):
+    def __init__(self, imagepath):
         # 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-app-id
         # 示例代码中 x-ti-app-id 非真实数据
         self._app_id = '374c737cff005324cf43396e3984c9a7'
         # 请登录后前往 “工作台-账号设置-开发者信息” 查看 x-ti-secret-code
         # 示例代码中 x-ti-secret-code 非真实数据
         self._secret_code = '3b773612518dab6f5d987dc65bce35e1'
-        self.image = image
+        self.image = get_file_content(imagepath)
 
     def recognize(self):
         # 通用表格识别
