dd9d3ee3da8b5e6224cff66d1d14e9fcd8efa85c
Bravo2333 <2087369435@qq.com>
2024-10-15 19:48:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,5 +113,8 @@ def static_files(filename):
 
 if __name__ == '__main__':
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # 自动创建表
     # 初始化 SQLAlchemy
     app.run(debug=True,port=3000)
